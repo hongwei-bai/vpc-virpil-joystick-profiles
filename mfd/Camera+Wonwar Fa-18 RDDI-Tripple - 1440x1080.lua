52c9c2d43df43f27aa31cc0c7457fcb40e5b2e15
Hongwei <bhw8412@gmail.com>
2022-04-03 23:36:43 +1000

    up mfd config

---
+++ mfd/Camera+Wonwar Fa-18 RDDI-Tripple - 1440x1080.lua
@@ -0,0 +1,26 @@
+_  = function(p) return p; end;
+name = _('Camera +Wonwar Fa-18 Tripple RDDI');
+Description = 'Dual Trial'
+Viewports =
+{
+     Center =
+     {
+          x = 0;
+          y = 0;
+          width  = 5760;
+          height = 1080;
+          viewDx = 0;
+          viewDy = 0;
+          aspect = 5.3333333333;
+     }
+}
+
+RIGHT_MFCD =
+{
+     x = 5760;
+     y = 0;
+     width = 1440;
+     height = 1080;
+}
+
+UIMainView = Viewports.Center
